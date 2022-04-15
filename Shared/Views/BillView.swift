0b9c6f2b9b0276a112a964a4fbd5734e71d2528f
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-15 15:08:20 +0900

    Alter the bill textfield in order to only get decimal numbers.

--- Shared/Views/BillView.swift
+++ Shared/Views/BillView.swift
@@ -6,9 +6,11 @@
 //
 
 import SwiftUI
+import Combine
 
 struct BillView: View {
     @State private var billString: String = ""
+    @FocusState private var isBillAmountTextFieldFocused: Bool
     var body: some View {
         VStack(alignment: .leading) {
             Text("Bill")
@@ -17,9 +19,26 @@ struct BillView: View {
                 .foregroundColor(Color("TitleColor"))
             TextField("Bill Amount", text: $billString)
                 .textFieldStyle(CalculatorTextFieldStyle())
+                .multilineTextAlignment(.trailing)
+                .keyboardType(.numbersAndPunctuation)
+                .focused($isBillAmountTextFieldFocused)
+                .onChange(of: isBillAmountTextFieldFocused) { isFocused in
+                    if !isFocused {
+                        
+                    }
+                }
+                .onReceive(Just(billString)) { newValue in
+                    let filteredString: String = newValue.filter {
+                        "012345679.".contains($0)
+                    }
+                    if filteredString != newValue {
+                        billString = filteredString
+                    }
+                }
         }
     }
 }
+
 
 struct BillView_Previews: PreviewProvider {
     static var previews: some View {
