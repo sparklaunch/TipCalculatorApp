82f7b787df24b4067f142fc2e2e0010569109efb
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-15 12:32:04 +0900

    Initial Commit

---
+++ Shared/TipCalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorAppApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/04/15.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
