70999f404e56ec296c0faa5fa3f9738f13d1655f
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-17 11:44:48 +0900

    Add the zero validator.

--- Shared/Views/Calculator/NumberOfPeopleView.swift
+++ Shared/Views/Calculator/NumberOfPeopleView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct NumberOfPeopleView: View {
     @FocusState private var isNumberOfPeopleFocused: Bool
+    @State private var isNumberOfPeopleZero: Bool = false
     @Binding var numberOfPeople: Double
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Number of People")
-                .font(.title2)
-                .fontWeight(.bold)
-                .foregroundColor(Color("TitleColor"))
+            HStack {
+                Text("Number of People")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("TitleColor"))
+                Spacer()
+                Text("Can't be zero")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("WarningColor"))
+                    .opacity(isNumberOfPeopleZero ? 1 : .zero)
+            }
             TextField("Number of People", value: $numberOfPeople, format: .number.sign(strategy: .never))
                 .textFieldStyle(NumberOfPeopleTextFieldStyle())
                 .multilineTextAlignment(.trailing)
@@ -27,6 +36,16 @@ struct NumberOfPeopleView: View {
                         .opacity(isNumberOfPeopleFocused ? 1 : .zero)
                         .animation(.default, value: isNumberOfPeopleFocused)
                 )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 15)
+                        .strokeBorder(Color("WarningColor"), lineWidth: 3)
+                        .opacity(isNumberOfPeopleZero ? 1 : .zero)
+                )
+                .onChange(of: isNumberOfPeopleFocused) { isFocused in
+                    withAnimation(.default) {
+                        isNumberOfPeopleZero = !isFocused && numberOfPeople == 0
+                    }
+                }
         }
     }
 }
